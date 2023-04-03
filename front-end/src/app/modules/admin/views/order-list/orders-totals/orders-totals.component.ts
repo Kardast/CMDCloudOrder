b3e970ec1415a233283860db510ab85f11d87400
Giuseppe2408 <giuseppe.acito.ga@gmail.com>
2023-04-03 18:05:33 +0200

    refactor front end

--- front-end/src/app/modules/admin/views/order-list/orders-totals/orders-totals.component.ts
+++ front-end/src/app/modules/admin/views/order-list/orders-totals/orders-totals.component.ts
@@ -1,50 +1,45 @@
 import { Component, Input } from '@angular/core';
-import { Order } from 'app/core/services/api.service';
-import { Observable } from 'rxjs';
+import { Order, OrderClient, OrderTime } from 'app/core/services/api.service';
+import { Observable, combineLatest, switchMap } from 'rxjs';
 
 @Component({
-    selector: 'app-orders-totals',
-    templateUrl: './orders-totals.component.html',
-    styleUrls: ['./orders-totals.component.scss']
+  selector: 'app-orders-totals',
+  templateUrl: './orders-totals.component.html',
+  styleUrls: ['./orders-totals.component.scss']
 })
 export class OrdersTotalsComponent {
-    @Input() orders$: Observable<Order[]>;
 
-    totalAssemblyTime = 0;
-    totalBendingTime = 0;
-    totalPreparationTime = 0;
-    totalCuttingTime = 0;
+  ordersDate$: Observable<OrderTime[]>;
+  totalDays : [];
+  totalAssemblyTime = 0;
+  totalBendingTime = 0;
+  totalPreparationTime = 0;
+  totalCuttingTime = 0;
 
-    ngOnChanges() {
-        this.updateTotals();
+  constructor(private orderClient: OrderClient) {
+    
+    orderClient.dateList().subscribe(() => {
+      this.ordersDate$ = orderClient.dateList();
     }
+    )
+  }
 
-    updateTotals() {
-        this.orders$.subscribe(orders => {
-            this.totalAssemblyTime = orders.reduce((acc) => {
-                return acc + 1;
-            }, 0);
-            this.totalPreparationTime = orders.reduce((acc, order) => {
-                const assemblyDate = new Date(order.assemblyDate);
-                const preparationDate = new Date(order.preparationDate);
-                return acc + this.getDiffDays(assemblyDate, preparationDate);
-            }, 0);
-            this.totalBendingTime = orders.reduce((acc, order) => {
-                const assemblyDate = new Date(order.assemblyDate);
-                const bendingDate = new Date(order.bendingDate);
-                return acc + this.getDiffDays(assemblyDate, bendingDate);
-            }, 0);
-            this.totalCuttingTime = orders.reduce((acc, order) => {
-                const assemblyDate = new Date(order.assemblyDate);
-                const cuttingDate = new Date(order.cuttingDate);
-                return acc + this.getDiffDays(assemblyDate, cuttingDate);
-            }, 0);
-        });
-    }
+  ngOnChanges() {
+    this.updateTotals();
+  }
 
-    getDiffDays(endDate: Date, startDate: Date) {
-        let diffTime = endDate.getTime() - startDate.getTime();
-        const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-        return diffDays;
-    }
+  updateTotals() {
+    this.orderClient.dateList().subscribe(() => {
+      this.ordersDate$ = this.orderClient.dateList();
+    })
+  }
 }
+    // this.orderClient.dateList().subscribe(() => {
+    //   this.ordersDate$ = this.orderClient.dateList().subscribe(order =>{
+    //     this.totalDays.push(order);
+    //     this.totalAssemblyTime = this.totalDays[0].assembly;
+    //     this.totalBendingTime = this.totalDays[0].assemblyBendingSum;
+    //     this.totalPreparationTime = this.totalDays[0].assemblyPrepSum;
+    //     this.totalCuttingTime = this.totalDays[0].assemblyCutSum;
+    //   });
+    // })
