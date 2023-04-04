517309607332459972501cea1c338ecf24ef2e6a
Kardast <dottorsf@gmail.com>
2023-04-04 10:57:03 +0200

    working on totals

--- front-end/src/app/modules/admin/views/order-list/orders-totals/orders-totals.component.ts
+++ front-end/src/app/modules/admin/views/order-list/orders-totals/orders-totals.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { Order, OrderClient, OrderTime } from 'app/core/services/api.service';
-import { Observable, combineLatest, switchMap } from 'rxjs';
+import { Component } from '@angular/core';
+import { OrderClient, OrderTime } from 'app/core/services/api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-orders-totals',
@@ -9,37 +9,23 @@ import { Observable, combineLatest, switchMap } from 'rxjs';
 })
 export class OrdersTotalsComponent {
 
-  ordersDate$: Observable<OrderTime[]>;
-  totalDays : [];
+  ordersDate$ = new Observable<OrderTime[]>;
+  totalDays = [];
+
   totalAssemblyTime = 0;
   totalBendingTime = 0;
   totalPreparationTime = 0;
   totalCuttingTime = 0;
 
   constructor(private orderClient: OrderClient) {
-    
-    orderClient.dateList().subscribe(() => {
-      this.ordersDate$ = orderClient.dateList();
-    }
-    )
-  }
-
-  ngOnChanges() {
-    this.updateTotals();
-  }
-
-  updateTotals() {
-    this.orderClient.dateList().subscribe(() => {
-      this.ordersDate$ = this.orderClient.dateList();
-    })
+    orderClient.dateList().subscribe(order => {
+      console.log("1 " + order);
+      this.totalDays.push(order);
+      console.log("2 " + this.totalDays[0].assembly);
+      this.totalAssemblyTime = this.totalDays[0].assembly;
+      this.totalBendingTime = this.totalDays[0].assemblyBendingSum;
+      this.totalPreparationTime = this.totalDays[0].assemblyPrepSum;
+      this.totalCuttingTime = this.totalDays[0].assemblyCutSum;
+    });
   }
 }
-    // this.orderClient.dateList().subscribe(() => {
-    //   this.ordersDate$ = this.orderClient.dateList().subscribe(order =>{
-    //     this.totalDays.push(order);
-    //     this.totalAssemblyTime = this.totalDays[0].assembly;
-    //     this.totalBendingTime = this.totalDays[0].assemblyBendingSum;
-    //     this.totalPreparationTime = this.totalDays[0].assemblyPrepSum;
-    //     this.totalCuttingTime = this.totalDays[0].assemblyCutSum;
-    //   });
-    // })
