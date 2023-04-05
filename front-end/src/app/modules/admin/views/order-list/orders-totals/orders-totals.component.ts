df593809a4d4599e0d98fe7c220c8ec5e0b1a288
Kardast <dottorsf@gmail.com>
2023-04-05 10:36:06 +0200

    totals

--- front-end/src/app/modules/admin/views/order-list/orders-totals/orders-totals.component.ts
+++ front-end/src/app/modules/admin/views/order-list/orders-totals/orders-totals.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { OrderClient, OrderTime } from 'app/core/services/api.service';
+import { Component, Input } from '@angular/core';
+import { Order, OrderClient, OrderTime } from 'app/core/services/api.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -8,24 +8,25 @@ import { Observable } from 'rxjs';
   styleUrls: ['./orders-totals.component.scss']
 })
 export class OrdersTotalsComponent {
+  @Input() orders$: Observable<Order[]>;
 
   ordersDate$ = new Observable<OrderTime[]>;
   totalDays = [];
 
-  totalAssemblyTime = 0;
-  totalBendingTime = 0;
-  totalPreparationTime = 0;
-  totalCuttingTime = 0;
-
   constructor(private orderClient: OrderClient) {
-    orderClient.dateList().subscribe(order => {
-      console.log("1 " + order);
+  }
+
+  ngOnChanges() {
+    this.orders$.subscribe(_ => {
+      this.ordersDate$ = this.orderClient.dateList();
+      this.updateTotals();
+    })
+  }
+
+  updateTotals() {
+    this.ordersDate$.subscribe(order => {
+      this.totalDays = [];
       this.totalDays.push(order);
-      console.log("2 " + this.totalDays[0].assembly);
-      this.totalAssemblyTime = this.totalDays[0].assembly;
-      this.totalBendingTime = this.totalDays[0].assemblyBendingSum;
-      this.totalPreparationTime = this.totalDays[0].assemblyPrepSum;
-      this.totalCuttingTime = this.totalDays[0].assemblyCutSum;
     });
   }
 }
