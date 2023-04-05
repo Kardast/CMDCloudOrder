789c76282feb6fcca557d024c494b41f4bbf329e
Dario <dario.ferrai>
2023-04-05 16:15:37 +0200

    Updating totals logic

--- back-end/CMDCloudOrder/Cqrs/Queries/GetDateQuery.cs
+++ back-end/CMDCloudOrder/Cqrs/Queries/GetDateQuery.cs
@@ -1,20 +1,21 @@
 using CMDCloudOrder.Data;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMDCloudOrder.Cqrs.Queries;
 
-public record GetDateQuery() : IRequest<GetDateQueryHandler.OrderTime>;
+public record GetDateQuery() : IRequest<GetDateQueryHandler.OrderTime?>;
 
-internal class GetDateQueryHandler : IRequestHandler<GetDateQuery, GetDateQueryHandler.OrderTime>
+internal class GetDateQueryHandler : IRequestHandler<GetDateQuery, GetDateQueryHandler.OrderTime?>
 {
     private readonly OrderDbContext _db;
 
     public class OrderTime
     {
-        public int Assembly { get; set; }
-        public int AssemblyCutSum { get; set; }
-        public int AssemblyPrepSum { get; set; }
-        public int AssemblyBendingSum { get; set; }
+        public int AssemblyTotal { get; set; }
+        public int CuttingTotal { get; set; }
+        public int PreparationTotal { get; set; }
+        public int BendingTotal { get; set; }
     }
 
     public GetDateQueryHandler(OrderDbContext db)
@@ -22,38 +23,23 @@ internal class GetDateQueryHandler : IRequestHandler<GetDateQuery, GetDateQueryH
         _db = db;
     }
 
-    public Task<OrderTime?> Handle(GetDateQuery request, CancellationToken ct)
+    public async Task<OrderTime?> Handle(GetDateQuery request, CancellationToken ct)
     {
-        var orders = _db.Orders.AsQueryable();
-        var days = new List<OrderTime>();
-        var assembly = 0;
-        var assemblyCutSum = 0;
-        var assemblyPrepSum = 0;
-        var assemblyBendingSum = 0;
+        var orders = await _db.Orders.AsQueryable().ToArrayAsync(ct);
+        var result = new OrderTime();
         foreach (var order in orders)
         {
-            if (order.AssemblyDate is not null && order.CuttingDate is not null && order.PreparationDate is not null &&
-                order.BendingDate is not null)
+            if (!order.AssemblyDate.HasValue)
             {
-                var assemblyCutDiff = order.AssemblyDate.Value.DayNumber - order.CuttingDate.Value.DayNumber;
-                var assemblyPrepDiff = order.AssemblyDate.Value.DayNumber - order.PreparationDate.Value.DayNumber;
-                var assemblyBendingDiff = order.AssemblyDate.Value.DayNumber - order.BendingDate.Value.DayNumber;
-
-                assembly++;
-                assemblyCutSum += assemblyCutDiff;
-                assemblyPrepSum += assemblyPrepDiff;
-                assemblyBendingSum += assemblyBendingDiff;
-
-                days.Add(new OrderTime()
-                {
-                    Assembly = assembly,
-                    AssemblyCutSum = assemblyCutSum,
-                    AssemblyPrepSum = assemblyPrepSum,
-                    AssemblyBendingSum = assemblyBendingSum
-                });
+                continue;
             }
+
+            result.AssemblyTotal++;
+            result.CuttingTotal += order.AssemblyDate.Value.DayNumber - order.CuttingDate.GetValueOrDefault(order.AssemblyDate.Value).DayNumber;
+            result.PreparationTotal += order.AssemblyDate.Value.DayNumber - order.PreparationDate.GetValueOrDefault(order.AssemblyDate.Value).DayNumber;
+            result.BendingTotal += order.AssemblyDate.Value.DayNumber - order.BendingDate.GetValueOrDefault(order.AssemblyDate.Value).DayNumber;
         }
 
-        return Task.FromResult(days.LastOrDefault());
+        return result;
     }
 }
