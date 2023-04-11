e2c1e4c755836e6cecc9821aebf5f5b1a39ed638
Dario <dario.ferrai>
2023-04-11 15:41:11 +0200

    wip

--- back-end/CMDCloudOrder/Cqrs/Queries/GetOrderByDateQuery.cs
+++ back-end/CMDCloudOrder/Cqrs/Queries/GetOrderByDateQuery.cs
@@ -1,6 +1,9 @@
+using System.Linq.Expressions;
 using CMDCloudOrder.Data;
+using CMDCloudOrder.Extensions;
 using CMDCloudOrder.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMDCloudOrder.Cqrs.Queries;
 
@@ -17,17 +20,38 @@ internal class GetOrderByDateQueryHandler : IRequestHandler<GetOrderByDateQuery,
 
     public Task<Order[]> Handle(GetOrderByDateQuery request, CancellationToken ct)
     {
-        return Task.FromResult(_db.Orders.AsEnumerable()
-            .Where(or =>
-                or.CuttingDate.HasValue && or.CuttingDate.Value.Month == request.Month
-                || or.PreparationDate.HasValue && or.PreparationDate.Value.Month == request.Month
-                || or.BendingDate.HasValue && or.BendingDate.Value.Month == request.Month
-                || or.AssemblyDate.HasValue && or.AssemblyDate.Value.Month == request.Month)
-            .Where(or =>
-                or.CuttingDate.HasValue && or.CuttingDate.Value.Year == request.Year
-                || or.PreparationDate.HasValue && or.PreparationDate.Value.Year == request.Year
-                || or.BendingDate.HasValue && or.BendingDate.Value.Year == request.Year
-                || or.AssemblyDate.HasValue && or.AssemblyDate.Value.Year == request.Year)
-            .ToArray());
+        //
+        // var filterExpressions = new Expression<Func<Order, bool>>[]
+        // {
+        //     or => or.CuttingDate!.Value.Month == request.Month,
+        //     or => or.PreparationDate!.Value.Month == request.Month,
+        //     or => or.BendingDate!.Value.Month == request.Month,
+        //     or => or.AssemblyDate!.Value.Month == request.Month
+        // };
+        //
+        // var yearExpressions = new Expression<Func<Order, bool>>[]
+        // {
+        //     or => or.CuttingDate!.Value.Year == request.Year,
+        //     or => or.PreparationDate!.Value.Year == request.Year,
+        //     or => or.BendingDate!.Value.Year == request.Year,
+        //     or => or.AssemblyDate!.Value.Year == request.Year
+        // };
+        
+        
+        var start = new DateOnly(request.Year, request.Month, 1);
+        var end = start.AddMonths(1).AddDays(-1);
+        var filterExpressions = new Expression<Func<Order, bool>>[]
+        {
+            or => or.CuttingDate!.Value >= start && or.CuttingDate!.Value <= end,
+            or => or.PreparationDate!.Value >= start && or.PreparationDate!.Value <= end,
+            or => or.BendingDate!.Value >= start && or.BendingDate!.Value <= end,
+            or => or.AssemblyDate!.Value >= start && or.AssemblyDate!.Value <= end
+        };
+        
+
+        return _db.Orders
+            .Where(filterExpressions.Or())
+            // .Where(yearExpressions.Or())
+            .ToArrayAsync(ct);
     }
 }
